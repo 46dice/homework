3f421fdc260007749819a658508e053e6eccc165
46dice <130773969+46dice@users.noreply.github.com>
2023-05-14 22:46:43 +0500

    Add files via upload

--- docs/js/main.js
+++ docs/js/main.js
@@ -7,3 +7,22 @@ $('.directions__filter-btn').on('click', function () {
     $(this).addClass('directions__filter-btn--active')
 })
 
+$('.team__slider').slick({
+    slidesToShow: 4,
+    lazyLoad: 'ondemand',
+    slidesToScroll: 1,
+    draggable: false,
+    arrows: false,
+    appendArrows: $('.team__slider-arrows'),
+    waitForAnimate: false,
+})
+
+$('.team-slider-prev').on('click', function (e) {
+    e.preventDefault()
+    $('.team__slider').slick('slickPrev')
+})
+$('.team-slider-next').on('click', function (e) {
+    e.preventDefault()
+    $('.team__slider').slick('slickNext')
+})
+
